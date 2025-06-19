c00649edc3e1325f91d041d81300a2b7839d79be
Minato <rickycamposa@gmail.com>
2025-06-18 21:27:39 -0600

    hosting 7

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
+const serviceAccount = require('/etc/secrets/firebase-key.json');
+
 
 // Inicializa Express
 const app = express();
@@ -11,18 +13,7 @@ app.use(cors({ origin: true }));
 
 // Inicializa Firebase Admin
 admin.initializeApp({
-  credential: admin.credential.cert({
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-  })
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
